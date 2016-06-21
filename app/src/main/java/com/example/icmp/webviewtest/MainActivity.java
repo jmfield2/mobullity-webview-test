7845776b6a646ef40797a96c5f240e288b5a541d
icmp <jmfield2@mail.usf.edu>
2016-06-20 23:03:12 -0400

    initial

---
+++ app/src/main/java/com/example/icmp/webviewtest/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.icmp.webviewtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webView);
+
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setGeolocationEnabled(true);
+
+        myWebView.setWebContentsDebuggingEnabled(true);
+
+        myWebView.loadUrl("https://maps.usf.edu/");
+    }
+}
